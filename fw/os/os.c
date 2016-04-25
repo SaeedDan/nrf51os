90d3d06b7df61238b22a690d914ac6751768d3f4
captainmohyal <vbb@Vipins-MacBook-Air.local>
2016-04-25 17:56:29 -0600

    Added handler for os events. To be implemented in application.

--- fw/os/os.c
+++ fw/os/os.c
@@ -19,23 +19,27 @@
 
 #include "configuration.h"
 
-#include "app.h"
 #include "ble.h"
 #include "ble_stack_handler_types.h"
 #include "nrf.h"
+#if defined(RTC_INCLUDE)
+   #include "nrf_drv_rtc.h"
+#endif
 #include "nordic_common.h"
+#include "os.h"
 #include "softdevice_handler.h"
-#include "nrf_drv_rtc.h"
 
+
+enum OS_EVENT event;
 #if defined(RTC_INCLUDE)
    const nrf_drv_rtc_t rtc = NRF_DRV_RTC_INSTANCE(1);
 #endif
+
 
 #if defined(RTC_INCLUDE)
    static uint32_t  rtc_init(void);
    static void rtc_evt_handler(nrf_drv_rtc_int_type_t int_type);
 #endif
-
 static void ble_event_handler(ble_evt_t* evt);
 
 
@@ -45,11 +49,15 @@ int main(void)
    SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_SYNTH_250_PPM, NULL);
    softdevice_ble_evt_handler_set(ble_event_handler);
 
+#if defined(RTC_INCLUDE)
    // Enable the RTC Timer.
    rtc_init();
+#endif
    
    while (1)
    {
+      os_handler(event, NULL);   // TODO: Define in app module.
+
       // Enter power saving mode and wait for more events.
       sd_app_evt_wait();
    }
