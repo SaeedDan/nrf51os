2fbf0eae1755e14fcd710b0bfd541e7fc403ee66
vipinbb <vbb@Vipins-MacBook-Air.local>
2016-05-08 01:38:24 -0600

    Added HW Configuration for Focuslite board.

--- fw/configuration.h
+++ fw/configuration.h
@@ -29,7 +29,7 @@
    #define MAX_CONN_INTERVAL                MSEC_TO_UNITS(650, UNIT_1_25_MS)           /**< Maximum acceptable connection interval (0.65 second). */
    #define SLAVE_LATENCY                    0                                          /**< Slave latency. */
    #define CONN_SUP_TIMEOUT                 MSEC_TO_UNITS(4000, UNIT_10_MS)            /**< Connection supervisory timeout (4 seconds). */ 
-   #define DEVICE_NAME                      "FocusLite"
+   #define DEVICE_NAME                      "Ball"
    #define APP_ADV_INTERVAL                 300                                        /**< The advertising interval (in units of 0.625 ms. This value corresponds to 25 ms). */
    #define APP_ADV_TIMEOUT_IN_SECONDS       180                                        /**< The advertising timeout in units of seconds. */
    #define SEC_PARAM_BOND                   0                                          /**< Perform bonding. */
